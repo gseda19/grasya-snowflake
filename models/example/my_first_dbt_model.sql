9f281c53eb437b689b0b21aae5427de6a39d7e11
Grace Seda <grace.seda@fishtownanalytics.com>
2024-12-11 02:04:27 +0000

    added linting

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -10,7 +10,7 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-    select 2 as id,
+    SELECT 2 as id,
     CURRENT_TIMESTAMP() AS updated_at,
    /* 3 as new_number,
     6 as third_number,
