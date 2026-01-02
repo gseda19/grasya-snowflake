c0494f2eda9f57834d18d017ad6acc9fbca06f73
Grace Seda <grace.seda@dbtlabs.com>
2026-01-02 09:26:40 +0000

    sdsds

--- models/bar.sql
+++ models/bar.sql
@@ -1,8 +1,7 @@
 {{ config (materialized='table') }}
 
 
-with foo_data as (
-    select * from {{ref('foo')}}),
+with foo_data as (select * from {{ref('foo')}}),
 baz_data as (select * from {{ref('baz')}} )
 
 select * from foo_data
