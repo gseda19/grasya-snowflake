10d5c9773cc10c1a1771071d763846392dd017b3
Grace Seda <grace.seda@dbtlabs.com>
2026-01-02 07:44:41 +0000

    dsfsf

--- models/bar.sql
+++ models/bar.sql
@@ -1,6 +1,5 @@
-{{ config(materialized='table') }}
-select
-    foo.id
-from {{ ref('foo') }} as foo
-inner join {{ ref('baz') }} as baz
-    on foo.id = baz.id
+{{ config (materialized='table') }}
+
+select * from {{ref('foo')}}
+union all
+select * from {{ref('baz')}}
