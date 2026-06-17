c8ef88e550c37416b6725ecad6435ae44553c369
Grace Seda <grace.seda@dbtlabs.com>
2026-06-17 06:35:02 +0000

    asssds

---
+++ models/int_billing_procedure_dim.sql
@@ -0,0 +1,5 @@
+select
+  1 as BILLING_PROCEDURE_EPIC_ID,
+  '2026-06-01'::timestamp as ROW_START_DTS,
+  'proc a' as BILLING_PROCEDURE_NM,
+  'proc a short' as BILLING_PROCEDURE_SHORT_NM
---
+++ snapshots/test_fusion_snap.sql
@@ -0,0 +1,5 @@
+{% snapshot test_fusion_snap %}
+
+SELECT 1 AS id, 'foo' AS name
+
+{% endsnapshot %}
